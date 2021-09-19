d10eb12da5b3229f4d978b8333892ed3b1b98dd6
Otto Beyer <oerbeyer@gmail.com>
2021-09-19 16:42:43 +0000

    Update

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,9 +11,13 @@ connection = pymysql.connect(host='localhost', user='', password='', db='Chinook
 try:
     # Run a query
     with connection.cursor() as cursor:
-        rows = [("Bob", 21, "1990-02-06 23:04:56"),("Jim", 56, "1955-05-09 13:12:45"),("Fred", 100, "1911-09-12 01:01:01")]
-        sql = "INSERT INTO Friends VALUES (%s, %s, %s);"
-        cursor.executemany(sql, rows)
+        
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        
+        # rows = [("Bob", 21, "1990-02-06 23:04:56"),("Jim", 56, "1955-05-09 13:12:45"),("Fred", 100, "1911-09-12 01:01:01")]
+        # sql = "INSERT INTO Friends VALUES (%s, %s, %s);"
+        # cursor.executemany(sql, rows)
+        
         # sql = "DELETE FROM Friends WHERE name = 'Nic';"
         # cursor.execute(sql)
         connection.commit()
