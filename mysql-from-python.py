37193b800c51171f6157cb6ff10710768d70f195
Otto Beyer <oerbeyer@gmail.com>
2021-09-19 16:17:01 +0000

    Cursor

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -9,12 +9,12 @@ import pymysql
 connection = pymysql.connect(host='localhost', user='', password='', db='Chinook')
 
 try:
-    #Run a query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
+    # Run a query
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor:
+            print(row)
 finally:
     # Close the connection regardless of whether the above was successful or not
     connection.close()
