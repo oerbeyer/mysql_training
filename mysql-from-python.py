5462401a0bf14da1c2fb6a393a9c501b3855432a
Otto Beyer <oerbeyer@gmail.com>
2021-09-19 16:46:48 +0000

    Alternative Update and Update Many

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,7 +12,15 @@ try:
     # Run a query
     with connection.cursor() as cursor:
         
-        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        rows = [(24, "Bob"),(56,"Jim")]
+        sql = "UPDATE Friends SET age = %s WHERE name = %s;"
+        cursor.executemany(sql, rows)
+
+        row = (23, "Bob")
+        sql = "UPDATE Friends SET age = %s WHERE name = %s;"
+        cursor.execute(sql, row)
+
+        # cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
         
         # rows = [("Bob", 21, "1990-02-06 23:04:56"),("Jim", 56, "1955-05-09 13:12:45"),("Fred", 100, "1911-09-12 01:01:01")]
         # sql = "INSERT INTO Friends VALUES (%s, %s, %s);"
