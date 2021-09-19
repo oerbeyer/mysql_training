40fb9d1e490f975bca6d98f13f2d6fa7f776ae22
Otto Beyer <oerbeyer@gmail.com>
2021-09-19 16:40:48 +0000

    Execute Many

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,10 +11,10 @@ connection = pymysql.connect(host='localhost', user='', password='', db='Chinook
 try:
     # Run a query
     with connection.cursor() as cursor:
-        row = ("Nic", 21, "1990-02-06 23:04:56")
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),("Jim", 56, "1955-05-09 13:12:45"),("Fred", 100, "1911-09-12 01:01:01")]
         sql = "INSERT INTO Friends VALUES (%s, %s, %s);"
+        cursor.executemany(sql, rows)
         # sql = "DELETE FROM Friends WHERE name = 'Nic';"
-        cursor.execute(sql, row)
         # cursor.execute(sql)
         connection.commit()
 finally:
